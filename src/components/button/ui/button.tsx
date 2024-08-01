9aa14039160c676fa763bc410835b4c21958231e
Alexander Khomenok <exleonardo@mail.ru>
2024-08-02 01:37:10 +0300

    feat: fix Button

--- src/components/button/ui/button.tsx
+++ src/components/button/ui/button.tsx
@@ -1,21 +1,20 @@
 'use client'
 import { PATH } from '@/common/routes'
-import { Events, State } from '@/store/projects'
+import store from '@/store/projects'
 import Cookies from 'js-cookie'
 import { useRouter } from 'next/navigation'
-import { useStoreon } from 'storeon/react'
 
 import s from '@/components/header/style/main-header.module.scss'
 
 export const Button = () => {
   const router = useRouter()
-  const { dispatch } = useStoreon<State, Events>('notification', 'globalMessage', 'status')
+
   const logoutHandler = () => {
     Cookies.remove('loggedin')
 
-    dispatch('globalMessage', 'logout')
-    dispatch('notification', true)
-    dispatch('status', 'pending')
+    store.dispatch('globalMessage', 'logout')
+    store.dispatch('notification', true)
+    store.dispatch('status', 'pending')
     router.push(PATH.base)
   }
 
