9973d709584b702106b428c86fd28445ebcba5dc
Alexander Khomenok <exleonardo@mail.ru>
2024-08-02 02:06:52 +0300

    feat: change TS ver 6

--- src/components/button/ui/button.tsx
+++ src/components/button/ui/button.tsx
@@ -1,13 +1,10 @@
 'use client'
 import { PATH } from '@/common/routes'
-import store, { EventsState, State } from '@/store/projects'
+import store from '@/store/projects'
 import Cookies from 'js-cookie'
 import { useRouter } from 'next/navigation'
-import { createStoreon } from 'storeon'
 
 import s from '@/components/header/style/main-header.module.scss'
-
-import DispatchEvent = createStoreon.DispatchEvent
 
 export const Button = () => {
   const router = useRouter()
@@ -17,7 +14,7 @@ export const Button = () => {
 
     store.dispatch<any>('globalMessage', 'logout' as any)
     store.dispatch<any>('notification', true as any)
-    store.dispatch<DispatchEvent<State, EventsState>>('status', 'pending')
+    store.dispatch<any>('status', 'pending' as any)
     router.push(PATH.base)
   }
 
